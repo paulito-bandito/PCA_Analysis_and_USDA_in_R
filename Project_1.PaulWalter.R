765a7b048de1338b66a7eba4860049f50c34e3db
Paul Walter <paul.walter.software@gmail.com>
2021-11-17 13:31:18 -0700

    Moved the logic for selecting the variables, finding those variables, and then pivoting said variables into their own method.

--- Project_1.PaulWalter.R
+++ Project_1.PaulWalter.R
@@ -28,6 +28,12 @@
   # Pivot Tables (wider)
   library(tidyr)
 
+
+  # PCA
+  library("FactoMineR")
+  library("factoextra")
+
+
   # ================================================================
   # METHODS
   # ================================================================@ 
@@ -43,52 +49,86 @@
     skewness(vectorToCheck, na.rm = TRUE)
   }
   
-  # ================================================================
-  # IDENTIFY VARIABLES (By Year and Type)
-  #
-  #   @see https://www.statology.org/filter-rows-r/
-  # ================================================================ 
-
-
-	
+  # OPEN, FILTER, and PIVOT.
+  openFilterAndPivot <- function( csvToOpenVariable, csvToOpenData, textInVariablesToSearch ) {
+    
+    # ================================================================
+    # IDENTIFY VARIABLES (By Year and Type)
+    #
+    #   @see https://www.statology.org/filter-rows-r/
+    # ================================================================ 
+    variableList <- read.csv(csvToOpenVariable) 
+    
+    filteredByType <- filter(variableList, Units %in% NUMERIC_VARIABLES )
+    
+    filteredByYear <- filter(filteredByType, grepl(textInVariablesToSearch, Variable_Name) )
+    
+    print(filteredByYear)
+    
+    filteredByYear_VariableCode = filteredByYear$Variable_Code
+    
+    filteredByYear_VariableCode
+    
+    # ================================================================
+    # FILTER DATA BY SELECTED VARIABLES
+    # ================================================================ 
+    dataStateAndCounty <- read.csv(csvToOpenData) 
+    dataStateAndCountyFiltered <- filter( dataStateAndCounty, Variable_Code %in% filteredByYear_VariableCode)
+    
+    # ================================================================
+    # PIVOT THE DATA
+    #
+    #   The data has a list of all the variables as rows, we need these as columns
+    #
+    #   @see https://tidyr.tidyverse.org/articles/pivot.html#wider
+    # ================================================================
+    pivoted <- pivot_wider(dataStateAndCountyFiltered, names_from = Variable_Code , values_from = Value)
+    # pivoted <- na.omit(pivoted)
+    # write.csv(pivoted, "data/output_pivoted.csv")
+  }
+  
+  
   #Set the number of numeric digits to work with
-	options(digits = 9)
+  options(digits = 9)
   
-  variableList <- read.csv("data/VariableList.csv") 
-	
-  filteredByType <- filter(variableList, Units %in% NUMERIC_VARIABLES )
+  #pivoted = openFilterAndPivot( "data/VariableList.csv", "data/StateAndCountyData.csv", "2015");
   
-  filteredByYear <- filter(filteredByType, grepl('2015', Variable_Name) )
+  pivoted = openFilterAndPivot( "data/VariableList.csv", "data/StateAndCountyData.csv", "2016");
   
-  filteredByYear_VariableCode = filteredByYear$Variable_Code
-  
-  filteredByYear_VariableCode
-  
-  # ================================================================
-  # FILTER DATA BY SELECTED VARIABLES
-  # ================================================================ 
-  dataStateAndCounty <- read.csv("data/StateAndCountyData.csv") 
-  dataStateAndCountyFiltered <- filter( dataStateAndCounty, Variable_Code %in% filteredByYear_VariableCode)
-  
-  # ================================================================
-  # PIVOT THE DATA
-  #
-  #   The data has a list of all the variables as rows, we need these as columns
-  #
-  #   @see https://tidyr.tidyverse.org/articles/pivot.html#wider
-  # ================================================================
-  pivoted <- pivot_wider(dataStateAndCountyFiltered, names_from = Variable_Code , values_from = Value)
-  #write.csv(pivoted, "data/output_pivoted.csv")
   # ================================================================
   # FILTER DATA BY ACCEPTABLE SKEWNESS
   # ================================================================ 
   
   # ignore the first 4 columns
   newdata <- pivoted[,4:ncol(pivoted)]
-  newdata
+  newdata <- na.omit(newdata) # Get rid of rows with no data.
+  
+  #sapply(newdata, checkSkewness )
+  
+  # PCA=princomp(newdata,
+  #              scale = TRUE, # this will scale the values that are too big.
+  #              cor = TRUE, 
+  #              scores = TRUE
+  # )
+  # 
+  # 
+  # summary(PCA)
+  # loadings(PCA)
+  # 
+  # 
+  # ## make a scree plot
+  # PCA.var <- PCA$sdev^2
+  # PCA.var.per <- round(PCA.var/sum(PCA.var)*100, 8) #To compute variance for a given PC, the eigenvalue for that PC must be divided by the sum of eigenvalues
+  # 
+  # barplot(PCA.var.per, main="Scree Plot", xlab="Principal Component", ylab="Percent Variation")
   
   # apply to all the columns https://stackoverflow.com/questions/7303322/apply-function-to-each-column-in-a-data-frame-observing-each-columns-existing-da
-  sapply(newdata, checkSkewness )
+  #newdata2 <-sapply(newdata, log10 )
+  #newdata2 <- na.omit(newdata2)
+  #write.csv(newdata2, "data/output_newdata2.csv")
+  #newdata2
+  #sapply(newdata2, checkSkewness )
+  
   
   # lapply(dataStateAndCountyFiltered$Value, checkSkewness)
   #by_county <- dataStateAndCountyFiltered %>% group_by(County)
